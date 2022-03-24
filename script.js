1d56b13eab9f377ab083169d3ff8e099277874bd
pabloantunamolina <97732734+pabloantunamolina@users.noreply.github.com>
2022-03-24 23:17:19 +0100

    changing rhinocompute.url for the final pinup

--- script.js
+++ script.js
@@ -105,8 +105,12 @@ rhino3dm().then(async m => {
 
     //RhinoCompute.url = getAuth( 'RHINO_COMPUTE_URL' ) // RhinoCompute server url. Use http://localhost:8081 if debugging locally.
     //RhinoCompute.apiKey = getAuth( 'RHINO_COMPUTE_KEY' )  // RhinoCompute server api key. Leave blank if debugging locally.
-    RhinoCompute.url = 'http://localhost:8081/' //if debugging locally.
     
+    //FOR LOCAL HOST
+    //RhinoCompute.url = 'http://localhost:8081/' //if debugging locally.
+    //FOR PRESENTATION IN MACAD SERVER. IT WILL STOP WORKING AFTER TH EPINUP, GET IT TO HEROKU IF YOU WANT.
+    RhinoCompute.url = "http://3.127.69.197/";
+    RhinoCompute.apiKey = "macad2022";
 
     // load a grasshopper file!
     const url = definitionName
